2b15c4040bac21ad28bb965f872131a6e6b4a347
Huy Tran <tmhuyy.itiu@gmail.com>
2023-08-14 11:32:56 +0700

    + create header, footer in home and expose header, footer

---
+++ home/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+const Footer = function () 
+{
+    return (
+        <div className="p-5 bg-blue-500 text-white text-3xl font-bold">
+            Only The Best Spinners
+        </div>
+    );
+};
+
+export default Footer;
