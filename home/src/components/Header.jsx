7a7bbb50611f2f6473b6b8ee2a0546446229c096
Huy Tran <tmhuyy.itiu@gmail.com>
2023-08-15 01:17:51 +0700

    + add error boundaries

--- home/src/components/Header.jsx
+++ home/src/components/Header.jsx
@@ -1,5 +1,4 @@
-const Header = function () 
-{
+const Header = function () {
     return (
         <div className="p-5 bg-blue-500 text-white text-3xl font-bold">
             Fidget Spinner World
--- product-detail-page/src/App.jsx
+++ product-detail-page/src/App.jsx
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import ReactDOM from "react-dom";
 
 import "./index.scss";
-import Header from "home/Header";
-import Footer from "home/Footer";
-
+import SafeComponent from "./components/SafeComponent";
+const Header = lazy(() => import("home/Header"));
+const Footer = lazy(() => import("home/Footer"));
+// import Header from "home/Header";
+// import Footer from "home/Footer";
 const App = () => (
     <div className="text-3xl mx-auto max-w-6xl">
-        <Header />
-        <div className="my-10">PDP Page Content</div>
-        <Footer />
+        <Suspense fallback={<div>Loading.....</div>}>
+            <SafeComponent>
+                <Header />
+            </SafeComponent>
+
+            <div className="my-10">PDP Page Content</div>
+            <Footer />
+        </Suspense>
     </div>
 );
 ReactDOM.render(<App />, document.getElementById("app"));
